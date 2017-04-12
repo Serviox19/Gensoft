53f01f60074f0200192ffe3cec42bcbfdb26be73
Servio Mora <servio.mora19@gmail.com>
2017-04-12 18:46:00 -0400

    ready to rumble

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(express.static("public"));
 app.use("/public", express.static(__dirname + "/public"));

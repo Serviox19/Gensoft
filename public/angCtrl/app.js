62fbca7c6c2489391b5911c57f2fe9180c48a15a
Servio Mora <servio.mora19@gmail.com>
2017-04-12 18:23:04 -0400

    completed angular setup

--- public/angCtrl/app.js
+++ public/angCtrl/app.js
@@ -1,3 +1,25 @@
-var app = angular.module('gensoft', []);
+var app = angular.module('gensoft', ['ui.router']);
 
-app.config()
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: "/public/partials/login.html",
+      controller: ""
+    })
+    .state('dash', {
+      url: "/home",
+      templateUrl: "/public/partials/dashboard.html",
+      controller: ""
+    })
+    .state('signup', {
+      url: "/signup",
+      templateUrl: "/public/partials/signup.html",
+      controller: ""
+    })
+
+
+  $locationProvider.html5Mode(true);
+});
--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
+app.use(express.static("public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('*', function (req, res) {
+  res.sendFile(process.cwd() + '/public/index.html');
+});
+
 
 server.listen(PORT, function () {
   console.log('Listening on PORT: ' + PORT);
